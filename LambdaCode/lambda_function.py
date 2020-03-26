af2252536a94c63a4590b5e7f082d09f758566e0
Nishant Jain <nishantjain@GTT-MacBook-Pro.local>
2020-03-26 16:21:10 -0500

    Changing Lambda

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,21 @@
-import boto3
-import json
-
-ec2 = boto3.client('ec2')
+import json 
+import datetime
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+    # TODO implement 
+    print(event)
+    data = {
+        'output': 'Hello from '+ event['Country'],
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
+
+# import boto3
+# import json
+
+# ec2 = boto3.client('ec2')
+# def lambda_handler(event, context):
+#     print(event)
+#     response = ec2.describe_availability_zones()
+#     return {"statusCode": 200, "body": json.dumps(response)}
